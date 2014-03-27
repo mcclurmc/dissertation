0180b6d56ae62d1627cfab9b96677cf4fe87672a
Mike McClurg <michael.mcclurg@kellogg.ox.ac.uk>
2014-03-27 15:44:59 -0600

    Finished draft of intro and background
    Also added papers: evolving jmock dsl, ieee dictionary.
    
    Signed-off-by: Mike McClurg <michael.mcclurg@kellogg.ox.ac.uk>

--- src/code/criterion_example.hs
+++ src/code/criterion_example.hs
@@ -9,7 +9,7 @@ fib n = fib (n-1) + fib (n-2)
 
 main = defaultMain [
   bgroup "fib" [ bench "10" $ whnf fib 10
-               , bench "35" $ whnf fib 35
-               , bench "37" $ whnf fib 37
+               , bench "20" $ whnf fib 35
+               , bench "25" $ whnf fib 37
                ]
   ]
